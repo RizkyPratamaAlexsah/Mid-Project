7ba2bc4f5770578763bc34b7cfa1861d991f6d34
RizkyPratamaAlexsah <90378774+RizkyPratamaAlexsah@users.noreply.github.com>
2022-02-26 07:46:08 +0700

    Update 2022_02_25_122819_create_book_table.php

--- database/migrations/2022_02_25_122819_create_book_table.php
+++ database/migrations/2022_02_25_122819_create_book_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('book', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('Title');
             $table->date('Year Published');
