d4da44f8884747d0b8d75754400362a62ada9353
RizkyPratamaAlexsah <90378774+RizkyPratamaAlexsah@users.noreply.github.com>
2022-02-26 07:46:09 +0700

    Update addController.php

--- app/Http/Controllers/addController.php
+++ app/Http/Controllers/addController.php
@@ -12,7 +12,7 @@ class addController extends Controller
     }
 
     public function storeBook(Request $request){
-        Book::create([
+        Books::create([
             'Title' => $request->bookTitle,
             'Year Published' => $request->yearPublished,
             'Author' => $request->author,
